cfd325abfbef86cf3559537c501cfaf634b3da49
camalzade_elvin <camalzade_elvin@outlook.com>
2019-12-29 08:33:43 -0800

    Add project files.

---
+++ ValidationProject/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ValidationProject.Models;
+
+namespace ValidationProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("Register",new Register() { Birthdate=DateTime.Now});
+        }
+        [HttpPost]
+        public IActionResult Register(Register model)
+        {
+            return View("Completed", model);
+        }
+    }
+}
---
+++ ValidationProject/Models/Register.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ValidationProject.Models
+{
+    public class Register
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        [UIHint("Date")]
+        public DateTime Birthdate { get; set; }
+        public bool TermsAccepted { get; set; }
+    }
+}
